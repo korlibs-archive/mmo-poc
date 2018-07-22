58955630e56ef6c8669d78f70dfbf7295ddaf53e
soywiz <soywiz@gmail.com>
2018-07-22 22:02:09 +0200

    Some packing adjustments

--- mmo/jvm/src/mmo/server/util/MiniWebpack.kt
+++ mmo/jvm/src/mmo/server/util/MiniWebpack.kt
@@ -2,6 +2,14 @@ package mmo.server.util
 
 import com.soywiz.korio.file.*
 import java.util.LinkedHashMap
+import kotlin.collections.LinkedHashSet
+import kotlin.collections.List
+import kotlin.collections.arrayListOf
+import kotlin.collections.filter
+import kotlin.collections.first
+import kotlin.collections.getOrPut
+import kotlin.collections.isNotEmpty
+import kotlin.collections.plusAssign
 
 class MiniWebpack {
     private val requireRegex = Regex("require\\('(.*?)'\\)")
@@ -31,6 +39,7 @@ class MiniWebpack {
             }
         }
 
+        // Simple packing for UMD packages
         return buildString {
             for (mod in topologySort(depEdges).filter { it in validModules }) {
                 //println("- $mod")
@@ -39,31 +48,26 @@ class MiniWebpack {
             }
         }
     }
-}
 
-
-suspend fun VfsFile.miniWebpack(): String {
-    val res = MiniWebpack().miniWebpack(this.parent, this.basenameWithoutCompoundExtension)
-    //println(res)
-    //println("Size: ${res.toByteArray().size} bytes")
-    //System.exit(-1)
-    return res
-}
-
-fun topologySort(edges: List<Pair<String, String>>): List<String> {
-    val graph = LinkedHashMap<String, LinkedHashSet<String>>()
-    val out = arrayListOf<String>()
-    for (edge in edges) {
-        graph.getOrPut(edge.second) { LinkedHashSet() }
-        graph.getOrPut(edge.first) { LinkedHashSet() }.add(edge.second)
-    }
-    while (graph.isNotEmpty()) {
-        val emptyDep = graph.filter { it.value.isEmpty() }.keys.first()
-        out += emptyDep
-        graph.remove(emptyDep)
-        for (set in graph.values) {
-            set.remove(emptyDep)
+    private fun topologySort(edges: List<Pair<String, String>>): List<String> {
+        val graph = LinkedHashMap<String, LinkedHashSet<String>>()
+        val out = arrayListOf<String>()
+        for (edge in edges) {
+            graph.getOrPut(edge.second) { LinkedHashSet() }
+            graph.getOrPut(edge.first) { LinkedHashSet() }.add(edge.second)
         }
+        while (graph.isNotEmpty()) {
+            val emptyDep = graph.filter { it.value.isEmpty() }.keys.first()
+            out += emptyDep
+            graph.remove(emptyDep)
+            for (set in graph.values) {
+                set.remove(emptyDep)
+            }
+        }
+        return out
     }
-    return out
 }
+
+
+suspend fun VfsFile.miniWebpack(): String =
+    MiniWebpack().miniWebpack(this.parent, this.basenameWithoutCompoundExtension)
